91efd2ada8b8bd55d7c8219fee590c150d843941
Thomas Stäheli <thomas.staheli@hes-so.ch>
2025-08-21 11:26:05 +0200

    pin toggle

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -100,6 +100,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	  HAL_Delay(200);
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+	  HAL_Delay(200);
   }
   /* USER CODE END 3 */
 }
